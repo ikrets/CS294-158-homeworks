c9b86c588fc726e297e73a13f3d29dd4d554d1b9
Ilia Krets <mrchair2170@gmail.com>
2019-10-08 22:40:10 +0200

    Add batch norm and relu activations between convs

--- task_2_2/transformations.py
+++ task_2_2/transformations.py
@@ -6,15 +6,28 @@ tfkl = tf.keras.layers
 
 def simple_resnet(filters, blocks, channels, name='simple_resnet'):
     input = tfkl.Input(shape=[None, None, channels])
-    net = tfkl.Conv2D(filters=filters, kernel_size=3, padding='same', name=f'{name}/initial_conv')(input)
-    for i in range(blocks):
-        net = tfkl.Conv2D(filters=filters, kernel_size=1, activation='relu', name=f'{name}/block_{i}_conv_0')(net)
-        prelim = tfkl.Conv2D(filters=filters, kernel_size=3, padding='same', name=f'{name}/block_{i}_conv_1')(net)
-        net = tfkl.Activation('relu')(prelim)
-        net = tfkl.Conv2D(filters=filters, kernel_size=3, padding='same', name=f'{name}/block_{i}_conv_2')(net)
-        net = tfkl.Add()([prelim, net])
-
+    net = tfkl.Conv2D(filters=filters, kernel_size=3, padding='same', use_bias=False, name=f'{name}/initial_conv')(
+        input)
+    net = tfkl.BatchNormalization()(net)
     net = tfkl.Activation('relu')(net)
+
+    for i in range(blocks):
+        block_input = net
+
+        net = tfkl.Conv2D(filters=filters, kernel_size=1, use_bias=False, name=f'{name}/block_{i}_conv_0')(net)
+        net = tfkl.BatchNormalization()(net)
+        net = tfkl.Activation('relu')(net)
+
+        net = tfkl.Conv2D(filters=filters, kernel_size=3, use_bias=False, padding='same',
+                          name=f'{name}/block_{i}_conv_1')(net)
+        net = tfkl.BatchNormalization()(net)
+        net = tfkl.Activation('relu')(net)
+
+        net = tfkl.Conv2D(filters=filters, kernel_size=1, use_bias=False, name=f'{name}/block_{i}_conv_2')(net)
+        net = tfkl.BatchNormalization()(net)
+        net = tfkl.Add()([block_input, net])
+        net = tfkl.Activation('relu')(net)
+
     net = tfkl.Conv2D(filters=channels * 2, kernel_size=3, padding='same', name=f'{name}/final_conv')(net)
     net = tfkl.Lambda(lambda X: tf.split(X, 2, axis=-1))(net)
 
